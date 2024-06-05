016b2a35e855e28a83b4c48b2182e885811d1a9e
Lum1n53 <hxiao60@stuy.edu>
2024-06-05 15:31:47 -0400

    a

--- start.py
+++ start.py
@@ -28,7 +28,7 @@ if ('name' in data):
     name = data['name'].value
 guide = 'Chiron'
 if ('guide' in data):
-    bgcolor = data['bgcolor'].value
+    bgcolor = data['guide'].value
 
 html= HTML_HEADER
 html+= '<h1>Welcome to Camp-Half-Blood ' + name + '</h1>'
